d1bf4378d07d75b676c425b774d5025d42bb4cb7
orneryhippo <jesse.cassill@gmail.com>
2023-12-18 13:16:44 -0500

    added a form interface

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Form, Depends
+from fastapi.responses import HTMLResponse
 from bs4 import BeautifulSoup
 import httpx
 import logging
+from pydantic import BaseModel
+
+class UrlModel(BaseModel):
+    url: str
+
 
 # Setting up logging to track application behavior
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -12,11 +18,31 @@ app = FastAPI()
 # Defining a constant for the maximum number of redirects
 MAX_REDIRECTS = 10
 
+@app.get("/", response_class=HTMLResponse)
+async def read_form():
+    with open("index.html", 'r') as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content)
+
+# ... (rest of your FastAPI code)
+
+
+
 @app.post("/scrape-url/")
-async def scrape_url(request: Request):
-    # Extracting the URL from the form data
-    form_data = await request.form()
-    url = form_data.get("url")
+async def scrape_url(form_data: UrlModel = Depends(), url: str = Form(None)):
+    # Decide which data source to use
+    target_url = url if url else form_data.url
+
+#     # Now 'target_url' contains the URL from either the form or a JSON request
+#     # ... process the target_url ...
+
+#     return {"message": "URL processed", "url": target_url}
+
+# @app.post("/scrape-url/")
+# async def scrape_url(request: Request):
+#     # Extracting the URL from the form data
+#     form_data = await request.form()
+#     url = form_data.get("url")
 
     # Validating the URL
     if not url:
@@ -51,63 +77,3 @@ async def scrape_url(request: Request):
         # General error handling
         logging.error(f'Error processing URL: {url}, Error: {str(e)}')
         raise HTTPException(status_code=500, detail="Internal server error")
-# from fastapi import FastAPI, HTTPException, Request
-# from bs4 import BeautifulSoup
-# import httpx
-# import logging
-# from databases import Database
-
-# # Database configuration
-# DATABASE_URL = "postgresql://user:password@localhost/dbname"
-# database = Database(DATABASE_URL)
-
-# # Setting up logging
-# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-# # Initializing the FastAPI application
-# app = FastAPI()
-
-# @app.on_event("startup")
-# async def startup():
-#     await database.connect()
-
-# @app.on_event("shutdown")
-# async def shutdown():
-#     await database.disconnect()
-
-# # Define a constant for the maximum number of redirects
-# MAX_REDIRECTS = 10
-
-# @app.post("/scrape-url/")
-# async def scrape_url(request: Request):
-#     form_data = await request.form()
-#     url = form_data.get("url")
-
-#     if not url:
-#         raise HTTPException(status_code=400, detail="URL is required")
-
-#     try:
-#         with httpx.Client(follow_redirects=True, max_redirects=MAX_REDIRECTS) as client:
-#             response = client.get(url)
-#             if response.is_redirect:
-#                 logging.info(f'Redirected to {response.url}')
-#             soup = BeautifulSoup(response.content, 'html.parser')
-#             paragraphs = [p.get_text() for p in soup.find_all('p')]
-#             links = [a['href'] for a in soup.find_all('a', href=True)]
-
-#             # Save data to database before returning
-#             await save_scraped_data(url, paragraphs, links)
-
-#             return {"paragraphs": paragraphs, "links": links}
-#     except httpx.TooManyRedirects:
-#         logging.error(f'Too many redirects encountered for URL: {url}')
-#         raise HTTPException(status_code=400, detail="Too many redirects")
-#     except Exception as e:
-#         logging.error(f'Error processing URL: {url}, Error: {str(e)}')
-#         raise HTTPException(status_code=500, detail="Internal server error")
-
-# async def save_scraped_data(url, paragraphs, links):
-#     query = "INSERT INTO your_table_name (url, paragraphs, links) VALUES (:url, :paragraphs, :links)"
-#     await database.execute(query, {"url": url, "paragraphs": paragraphs, "links": links})
-
-# Additional routes and logic can be added here...
